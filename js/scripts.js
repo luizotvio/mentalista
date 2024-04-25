1166884918331cbedf9e45dc4d474e75be6a2542
luizotvio <luizotavio.03@outlook.com>
2024-04-25 15:00:44 -0300

    feat: script.js atualizado para lógica do jogo funcionar junto com o DOM, style.css também foi atualizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
-var numeroSecreto = parseInt(Math.random() * 1001);
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
-while (chute != numeroSecreto) {
-    var chute = prompt("Digite um número entre 0 e 1000");
+let limiteTentativas = 5;
+let tentativas = 1;
 
-    chute = parseInt(chute);
+function playGame() {
+    let chute = parseInt(document.getElementById('chute').value);
 
-    if (chute === numeroSecreto) {
-        alert("Parabéns, você acertou!!!")
-    } else if (chute > numeroSecreto) {
-        alert("Você errou!!! o número secreto é menor")
+    tentativas++;
+
+    if (tentativas > limiteTentativas) {
+        document.getElementById('title-page').textContent = `Você usou todas as ${limiteTentativas} tentativas...`;
+        document.getElementById('feedback').textContent = 'Você perdeu!! Não desanime, você pode iniciar um novo jogo!';
+        document.getElementById('feedback').style.color = 'red';
+        document.getElementById('chute').disabled = true;
+    } else if (chute === numeroSecreto) {
+        document.getElementById('title-page').textContent = `Você acertou com ${tentativas} tentativas...`;
+        document.getElementById('feedback').textContent = 'Parabéns, você GANHOU!!!';
+        document.getElementById('feedback').style.color = 'green';
+        document.getElementById('chute').disabled = true;
     } else if (chute < numeroSecreto) {
-        alert("Você errou!!! o número secreto é maior")
+        document.getElementById('title-page').textContent = `Tentativa ${tentativas}, você ainda tem ${limiteTentativas - tentativas} tentativas.`;
+        document.getElementById('feedback').textContent = 'Você errou!!! o número secreto é maior';
+    } else {
+        document.getElementById('title-page').textContent = `Tentativa ${tentativas}, você ainda tem ${limiteTentativas - tentativas} tentativas.`;
+        document.getElementById('feedback').textContent = 'Você errou!!! o número secreto é menor';
     }
+}
+
+function newGame() {
+    document.getElementById('title-page').textContent = 'Adivinhe um número entre 1 e 100';
+    document.getElementById('feedback').textContent = 'Você tem 5 chances de acertar!';
+    document.getElementById('feedback').style.color = '';
+    document.getElementById('chute').disabled = false;
+    document.getElementById('chute').value = '';
+    tentativas = 1;
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
 }
